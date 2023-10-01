57f9298140f77b4eede6e2c858e11849786ac600
Nicolo <nicolo.frison97@gmail.com>
2023-10-01 11:15:05 -0400

    [Backend] Setup typescript for express
    closes #5

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
-const express = require( "express" );
+import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get( "/", ( req: express.Request, res: express.Response ) => {
     res.send( "Hello world!" );
 } );
 
