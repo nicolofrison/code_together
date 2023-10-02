cb86d9b89495fa6d3dfff110ea029940df67286b
Nicolo <nicolo.frison97@gmail.com>
2023-10-02 13:33:26 -0400

    [Backend] Fix database config and initialized the connection before start listening on express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
+import 'reflect-metadata';
 import Server from './server';
 
 // controllers instances
 import { authControllerInstance } from './controllers/auth.controller';
+import { appDataSource } from './config/dataSource';
 
 (async () => {
+  try {
+    await appDataSource.initialize();
+  } catch (error) {
+    console.error('Error while connecting to the database', error);
+    return error;
+  }
   const app = new Server([authControllerInstance]);
   app.listen();
 
