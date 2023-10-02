6ca4a1ac5a3d2a9bf4c8787e42baa7e2ec74e28d
Nicolo <nicolo.frison97@gmail.com>
2023-10-02 13:39:12 -0400

    [Backend] Created signUp endpoint

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,12 +1,17 @@
 /* eslint no-console: ["error", { allow: ["info", "error"] }] */
 import * as express from 'express';
 import Controller from '../models/http/controller';
-import { userService } from '../services/user.service';
+import { userService as userServiceInstance } from '../services/user.service';
+import validationMiddleware from '../middlewares/validation.middleware';
+import AuthPost from '../models/http/requests/authPost';
+import ServerHttpError from '../models/http/errors/ServerHttpError';
+import HttpError from '../models/http/errors/HttpError';
+import RecordNotFoundError from '../models/exceptions/RecordNotFoundError';
 
 class AuthController extends Controller {
   private static readonly PATH = '/auth';
 
-  public userService = userService;
+  private userService = userServiceInstance;
 
   public constructor() {
     super();
@@ -14,12 +19,36 @@ class AuthController extends Controller {
   }
 
   protected initRoutes() {
-    this.router.post(`${AuthController.PATH}/signup`, this.signUp);
+    this.router.post(
+      `${AuthController.PATH}/signup`,
+      validationMiddleware(AuthPost),
+      this.signUp
+    );
   }
 
-  private async signUp(request: express.Request, response: express.Response) {
-    response.send('Hello world');
-  }
+  private signUp = async (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const authData: AuthPost = request.body;
+
+    try {
+      const createdUser = await this.userService.createUser(authData);
+
+      if (createdUser != null) {
+        response.status(200);
+        response.send(createdUser);
+      }
+    } catch (e) {
+      if (e instanceof RecordNotFoundError) {
+        next(new HttpError(400, e.message, ''));
+      } else {
+        console.error(e);
+        next(new ServerHttpError());
+      }
+    }
+  };
 }
 
 export default AuthController;
