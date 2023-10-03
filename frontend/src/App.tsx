20ebe61b4ee513f66dbbb34df6272bd4cf1e3257
Nicolo <nicolo.frison97@gmail.com>
2023-10-02 21:45:17 -0400

    [Frontend] Moved CodeEditorWithSyntax to components/CodeEditorWithSyntax folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { CodeEditorWithSyntax } from './components/CodeEditorWithSyntax';
+import { CodeEditorWithSyntax } from './components/CodeEditorWithSyntax/CodeEditorWithSyntax';
 
 function App() {
   return (
