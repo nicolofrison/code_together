20919286d648d6ad2d16516204c9c73b162f0fca
Nicolo <nicolo.frison97@gmail.com>
2023-10-13 19:40:49 -0400

    [Frontend] Created the dialog to select if create or join a code session

--- frontend/src/components/CodeEditorWithSyntax/CodeEditorWithSyntax.tsx
+++ frontend/src/components/CodeEditorWithSyntax/CodeEditorWithSyntax.tsx
@@ -16,9 +16,13 @@ import './CodeEditorWithSyntax.css';
 import { CodeData } from '../../models/interfaces/webSocketMessage.interface';
 import WebSocketService from '../../services/webSocket.service';
 import { AuthContext } from '../AuthContext';
+import CreateJoinSharedCodeDialog from '../Utils/CreateJoinSharedCodeDialog';
+import { Grid } from '@mui/material';
 
 export function CodeEditorWithSyntax(): JSX.Element {
   const [code, setCode] = useState(``);
+  const [token, setToken] = useState('');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [language, setLanguage] = useState('javascript');
   // used to keep the opacity when the select is expanded, otherwise it would lose it
   const [selectIsExpanded, setSelectIsExpanded] = useState(false);
@@ -32,8 +36,13 @@ export function CodeEditorWithSyntax(): JSX.Element {
       WebSocketService.getInstance().setOnCodeCallback((data: CodeData) => {
         setCode(data.text);
       });
+      setIsDialogOpen(true);
     }
   }, [isLoggedIn]);
+
+  const onTokenSubmit = (t: string) => {
+    setToken(t);
+  };
 
   const onChange: ChangeEventHandler = (
     e: React.ChangeEvent<HTMLTextAreaElement>
@@ -48,37 +57,55 @@ export function CodeEditorWithSyntax(): JSX.Element {
   };
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-      <Select
-        labelId="language-select-label"
-        className={'language-select ' + (selectIsExpanded ? 'expanded' : '')}
-        id="language-select"
-        value={language}
-        label="Language"
-        onOpen={() => setSelectIsExpanded(true)}
-        onClose={() => setSelectIsExpanded(false)}
-        onChange={(e) => setLanguage(e.target.value)}
-      >
-        {languages.map((l) => (
-          <MenuItem key={l} value={l}>
-            {l[0].toUpperCase() + l.slice(1)}
-          </MenuItem>
-        ))}
-      </Select>
-      <CodeEditor
-        value={code}
-        language={language}
-        placeholder={`Please enter ${language} code.`}
-        onChange={onChange}
-        padding={15}
-        style={{
-          fontSize: 12,
-          fontFamily:
-            'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-          width: '100%',
-          height: '100%'
-        }}
+    <>
+      <CreateJoinSharedCodeDialog
+        onSubmit={onTokenSubmit}
+        open={isDialogOpen}
+        handleClose={() => setIsDialogOpen(false)}
       />
-    </div>
+      <Grid
+        container
+        height="100%"
+        justifyContent="space-between"
+        alignItems="stretch"
+      >
+        <Grid item xs={8}>
+          <p>Connected to {token}</p>
+        </Grid>
+        <Grid item xs={4}>
+          <Select
+            labelId="language-select-label"
+            id="language-select"
+            value={language}
+            label="Language"
+            onOpen={() => setSelectIsExpanded(true)}
+            onClose={() => setSelectIsExpanded(false)}
+            onChange={(e) => setLanguage(e.target.value)}
+          >
+            {languages.map((l) => (
+              <MenuItem key={l} value={l}>
+                {l[0].toUpperCase() + l.slice(1)}
+              </MenuItem>
+            ))}
+          </Select>
+        </Grid>
+        <Grid item xs>
+          <CodeEditor
+            value={code}
+            language={language}
+            placeholder={`Please enter ${language} code.`}
+            onChange={onChange}
+            padding={15}
+            style={{
+              fontSize: 12,
+              fontFamily:
+                'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+              width: '100%',
+              height: '100%'
+            }}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 }
---
+++ frontend/src/components/Utils/CreateJoinSharedCodeDialog.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { TextField } from '@mui/material';
+
+enum CodeAction {
+  CREATE,
+  JOIN
+}
+
+type Props = {
+  onSubmit: (token: string) => void;
+  open: boolean;
+  handleClose: () => void;
+};
+
+const defaultCode = (Math.floor(Math.random() * 999999) + 1).toPrecision(6);
+
+export default function AlertDialog(props: Props) {
+  const { onSubmit, open, handleClose } = props;
+
+  const [token, setToken] = useState(defaultCode);
+
+  const isTokenValid = (t: string) => {
+    const reg = new RegExp('[0-9]{6}');
+    return reg.test(t);
+  };
+
+  const [error, setError] = useState('');
+  const handleValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError(
+      isTokenValid(e.target.value)
+        ? ''
+        : 'The token should be composed by exactly 6 numbers'
+    );
+    setToken(e.target.value);
+  };
+
+  const setAction = (action: CodeAction) => {
+    if (action === CodeAction.CREATE) {
+      if (token === defaultCode) {
+        onSubmit(token);
+        handleClose();
+      } else {
+        setToken(defaultCode);
+      }
+    } else {
+      if (token === defaultCode) {
+        setToken('');
+      } else if (isTokenValid(token)) {
+        onSubmit(token);
+        handleClose();
+      }
+    }
+  };
+
+  const checkDisable = () => {
+    console.log(token);
+    console.log(defaultCode);
+    return token === defaultCode;
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Create or Join a code session?
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="token"
+            label="Token"
+            fullWidth
+            variant="standard"
+            type="numeric"
+            disabled={checkDisable()}
+            onChange={handleValidation}
+            error={error ? true : false}
+            helperText={error}
+            value={token}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            disabled={!isTokenValid(token)}
+            onClick={() => setAction(CodeAction.JOIN)}
+          >
+            Join
+          </Button>
+          <Button onClick={() => setAction(CodeAction.CREATE)} autoFocus>
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
