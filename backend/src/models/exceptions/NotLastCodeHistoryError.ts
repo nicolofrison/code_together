a0938a99a08ddab13ecf64b6ad33fab335b9960d
Nicolo <nicolo.frison97@gmail.com>
2023-10-18 13:54:56 -0400

    [Backend] Created CodeHistory controller and tests and edited NotLastCodeHistoryError text
    closes #20

--- backend/src/models/exceptions/NotLastCodeHistoryError.ts
+++ backend/src/models/exceptions/NotLastCodeHistoryError.ts
@@ -2,6 +2,8 @@ import CodeHistory from '../entities/CodeHistory';
 
 export default class NotLastCodeHistoryError extends Error {
   constructor() {
-    super(`The ${CodeHistory.name} with the id provided is not the last one`);
+    super(
+      `The ${CodeHistory.name} with the id provided is not the last one. Currently you can only delete the last one`
+    );
   }
 }
