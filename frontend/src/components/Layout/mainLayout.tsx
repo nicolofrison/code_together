24ee472d289c2a5355df109fc5b81392a9a38d2f
Nicolo <nicolo.frison97@gmail.com>
2023-10-26 15:41:11 -0400

    [Frontend] Set show chat only if isLoggedIn

--- frontend/src/components/Layout/mainLayout.tsx
+++ frontend/src/components/Layout/mainLayout.tsx
@@ -1,28 +1,35 @@
+import { useContext } from 'react';
+
 import { Grid } from '@mui/material';
 
+import { AuthContext } from '../AuthContext';
 import CardItem from './cardItem';
 import Chat from '../Chat/Chat';
 import { CodeEditorWithSyntax } from '../CodeEditorWithSyntax/CodeEditorWithSyntax';
 import LeftColumn from './leftColumn';
 
 export default function MainLayout() {
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
     <Grid container spacing={2} height="100%" style={{ marginTop: 0 }}>
-      <Grid item xs={12} md={3} lg={3} height="100%">
+      <Grid item xs={12} md={3} height="100%">
         <CardItem>
           <LeftColumn />
         </CardItem>
       </Grid>
-      <Grid item xs={12} md={6} lg={6} height="100%">
+      <Grid item xs={12} md={isLoggedIn ? 6 : 9} height="100%">
         <CardItem>
           <CodeEditorWithSyntax />
         </CardItem>
       </Grid>
-      <Grid item xs={12} md={3} lg={3} height="100%">
-        <CardItem>
-          <Chat />
-        </CardItem>
-      </Grid>
+      {isLoggedIn && (
+        <Grid item xs={12} md={3} height="100%">
+          <CardItem>
+            <Chat />
+          </CardItem>
+        </Grid>
+      )}
     </Grid>
   );
 }
